83e78ca59dc104b3b7ace04d3b853e5499fc72c5
Grumpy Cockatiel <tguillory@gmail.com>
2022-11-06 13:37:50 -0600

    Added a default function route

---
+++ Raydreams.MicroCMS/GetHome.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Raydreams.MicroCMS
+{
+    /// <summary>For now intercepts rquest to the top level and sends them to the home page</summary>
+    /// <remarks>Could be modified to get other types of files or to remove page completely</remarks>
+    public class GetHomeFunction : BaseFunction
+    {
+        public GetHomeFunction( ICMSGateway gate ) : base( gate )
+        { }
+
+        [Function( "GetHome" )]
+        public HttpResponseData Run( [HttpTrigger( AuthorizationLevel.Anonymous, "get", Route = "" )] HttpRequestData req, string file, FunctionContext ctx )
+        {
+            ILogger logger = ctx.GetLogger( "API" );
+            logger.LogInformation( $"{GetType().Name} triggered." );
+
+            return req.Redirect( $"{req.Url.Scheme}://{req.Url.Host}/page" );
+        }
+    }
+}
+
